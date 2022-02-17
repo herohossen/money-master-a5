b0f63fbffba06c030801d9f2ed2106d5bcf99365
herohossen <delowarhossen1224@gmail.com>
2022-02-17 16:02:25 +0600

    Save funtion added

--- js/script.js
+++ js/script.js
@@ -22,17 +22,70 @@ function getBalance(){
 
 }
 
+function getSaveAmount(income,percentage){
 
+  const getSaveAmount = (parseFloat(income)*parseFloat(percentage))/100;
+  return getSaveAmount;
+
+}
+
+function getRemainingAmount(totalBalance,saveAmount){
+
+  const getRemainingAmount = parseFloat(totalBalance)-parseFloat(saveAmount);
+  return getRemainingAmount;
+
+}
+
+function validationRule1(totalExpense,getIncome){
+  return parseFloat(totalExpense) > getIncome ? true : false;
+}
+
+function validationRule2(savingAmount,remainingAmount){
+  return savingAmount > remainingAmount ? true : false;
+}
 
 
 document.getElementById("calculate-button").addEventListener
 ("click",function(){
+//Calculate Total Expenses
 
-const totalExpenses=document.getElementById('total-expenses');
-totalExpenses.innerText=getExpenses();
-
+const totalExpense = getExpenses();
 const balance=getBalance();
+const isValidationFailed = validationRule1(totalExpense,balance);
+
+if(isValidationFailed){
+  alert('error message');
+  return false;
+}
+const totalExpenses=document.getElementById('total-expenses');
+totalExpenses.innerText = totalExpense;
+
+//Total Balance
+
 const currentBalance= getExpenses();
 const totalBalance=document.getElementById('total-balance');
 totalBalance.innerText=balance-currentBalance;
+});
+
+document.getElementById("amount-save-button").addEventListener
+("click",function(){
+//Calculate Total Expenses
+
+const getIncomeBalance = document.getElementById('income-input').value;
+const savingPercentage = document.getElementById('amount-input').value;
+const totalBalance = document.getElementById('total-balance').innerHTML;
+
+
+const saveAmount = getSaveAmount(getIncomeBalance,savingPercentage);
+const remainingAmount = getRemainingAmount(totalBalance,saveAmount);
+
+const isValidationFailed = validationRule2(saveAmount,remainingAmount);
+if(isValidationFailed){
+  alert('error message2');
+  return false;
+}
+document.getElementById('saving-amount').innerText=saveAmount;
+document.getElementById('remaining-balance').innerText=remainingAmount;
+
+
 });
